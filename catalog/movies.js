9cd4ff1e6b1067642385b8b6f3cfd3be421669c4
Roger0369 <roger0369reyes@gmail.com>
2025-05-04 17:54:46 -0600

    Add javascript vanilla movie hub

---
+++ catalog/movies.js
@@ -0,0 +1,30 @@
+const moviesCatalog = [
+    {
+    name: "Mad Max: Fury Road",
+    category: "action",
+    description: "En un futuro postapocalíptico, Max se une a una rebelde para huir de un tirano en una carrera de supervivencia.",
+    image: "./img/madmax_furyroad.webp",
+    infoPage: "./movies_files/madmax.html"
+  },
+  {
+    name: "John Wick",
+    category: "action",
+    description: "Un exasesino regresa al mundo del crimen para vengar la muerte de su perro, un regalo de su esposa fallecida.",
+    image: "./img/johnwick.webp",
+    infoPage: "./movies_files/johnwick.html"
+  },
+  {
+    name: "Deadpool",
+    category: "action",
+    description: "Un mercenario con habilidades regenerativas busca venganza mientras rompe la cuarta pared con humor sarcástico.",
+    image: "./img/deadpool.webp",
+    infoPage: "./movies_files/deadpool.html",
+  },
+  {
+    name: "Mickey 17",
+    category: "fiction",
+    description: "Mickey es clonado accidentalmente, dos versiones de su personalidad se ven obligadas a trabajar juntas.",
+    image: "./img/mickey17.webp",
+    infoPage: "./movies_files/mickey17.html"
+  },
+]
